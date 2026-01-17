171cbbe7ad7869fd65ed3366aaa7904a17bdf3af
dave95995 <danielv3234@outlook.com>
2026-01-17 12:58:10 +0100

    Add ManageParkingSpots

---
+++ Controllers/ManageParkingSpotsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using garage3.Data;
+
+namespace garage3.Controllers
+{
+    public class ManageParkingSpotsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ManageParkingSpotsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ManageParkingSpots
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ParkingSpots.ToListAsync());
+        }
+
+        // GET: ManageParkingSpots/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkingSpot = await _context.ParkingSpots
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parkingSpot == null)
+            {
+                return NotFound();
+            }
+
+            return View(parkingSpot);
+        }
+
+        // GET: ManageParkingSpots/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ManageParkingSpots/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SpotNumber,Size,IsBooked")] ParkingSpot parkingSpot)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parkingSpot);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parkingSpot);
+        }
+
+        // GET: ManageParkingSpots/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkingSpot = await _context.ParkingSpots.FindAsync(id);
+            if (parkingSpot == null)
+            {
+                return NotFound();
+            }
+            return View(parkingSpot);
+        }
+
+        // POST: ManageParkingSpots/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SpotNumber,Size,IsBooked")] ParkingSpot parkingSpot)
+        {
+            if (id != parkingSpot.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parkingSpot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParkingSpotExists(parkingSpot.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parkingSpot);
+        }
+
+        // GET: ManageParkingSpots/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkingSpot = await _context.ParkingSpots
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parkingSpot == null)
+            {
+                return NotFound();
+            }
+
+            return View(parkingSpot);
+        }
+
+        // POST: ManageParkingSpots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var parkingSpot = await _context.ParkingSpots.FindAsync(id);
+            if (parkingSpot != null)
+            {
+                _context.ParkingSpots.Remove(parkingSpot);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParkingSpotExists(int id)
+        {
+            return _context.ParkingSpots.Any(e => e.Id == id);
+        }
+    }
+}
